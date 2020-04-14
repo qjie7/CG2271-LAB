24ae60124b4b1037fd84b17f17a1189ab1d6b3ea
qjie777 <qianjie777@gmail.com>
2020-04-14 18:06:40 +0800

    FINAL CODE

--- CG2271 Project/RTE/_Target_1/myheader.h
+++ CG2271 Project/RTE/_Target_1/myheader.h
@@ -3,101 +3,66 @@
 #include "cmsis_os2.h"  
 #include <stdbool.h>
 
-//// For DRV8833 #1 
-//#define PTB0_PIN 										0    // tpm1 chn0
-//#define PTB1_PIN 										1 	 // tpm1 chn1
-//#define PTB2_PIN										2	   // tpm1 chn0
-//#define PTB3_PIN										3 	 // tpm1 chn1
-
-//// For DRV8833 #2
-//#define PTE30_PIN 									30   // tpm2 chn0
-//#define PTA5_PIN 										5		 // tpm2 chn1
-//#define PTC8_PIN										8    // tpm2 chn0
-//#define PTC9_PIN										9    // tpm2 chn1
-
+// For DRV8833 #1 
 #define PTE20_PIN 										20    // tpm1 chn0
-#define PTE21_PIN 										21 	 // tpm1 chn1
-#define PTB0_PIN											0	   // tpm1 chn0
-#define PTB1_PIN											1 	 // tpm1 chn1
+#define PTE21_PIN 										21 	  // tpm1 chn1
+#define PTB0_PIN											0	    // tpm1 chn0
+#define PTB1_PIN											1 	  // tpm1 chn1
 
 // For DRV8833 #2
-#define PTA1_PIN 										1   // tpm2 chn0
-#define PTA2_PIN 										2		 // tpm2 chn1
-#define PTB2_PIN										2    // tpm2 chn0
-#define PTB3_PIN										3    // tpm2 chn1
+#define PTA1_PIN 											1     // tpm2 chn0
+#define PTA2_PIN 											2		  // tpm2 chn1
+#define PTB2_PIN											2     // tpm2 chn0
+#define PTB3_PIN											3     // tpm2 chn1
 
-//For Green LED
-#define PTA13_PIN 									13
-#define PTC12_PIN 									12
-#define PTC13_PIN 									13
-#define PTC16_PIN 									16
-#define PTC17_PIN 									17
-#define PTD5_PIN 										5
-#define PTD0_PIN 										0
-#define PTD2_PIN 										2
+// For Green LED
+#define PTA13_PIN 										13
+#define PTC12_PIN 										12
+#define PTC13_PIN 										13
+#define PTC16_PIN 										16
+#define PTC17_PIN 										17
+#define PTD5_PIN 											5
+#define PTD0_PIN 											0
+#define PTD2_PIN 											2
 
 // For Red LED
-#define PTB8_PIN										8
+#define PTB8_PIN											8
 
 // For Buzzer 
-#define PTA4_PIN										4		// tpm0 chn 1
+#define PTA4_PIN											4			 // tpm0 chn1
 
 
 // For BT06/UART
-#define BAUD_RATE 									9600
-#define UART_TX_PORTE22 						22
-#define UART_RX_PORTE23 						23
-#define UART2_INT_PRIO 							128
+#define BAUD_RATE 										9600
+#define UART_TX_PORTE22 							22
+#define UART_RX_PORTE23 							23
+#define UART2_INT_PRIO 								128
 
 // Other Varaible Constants
-#define LOWERCOMMANDLIMIT						0
-#define UPPERCOMMANDLIMIT						15
-#define DEFAULT_duty_cycle						0.5f
-#define MSG_COUNT										1
+#define LOWERCOMMANDLIMIT							0
+#define UPPERCOMMANDLIMIT							15
+#define CONNECTED											100
+#define FINISHED											200
+#define STARTED												255
+#define MSG_COUNT											1
+
 
 // Shifting Operation
-#define MASK(x) 										(1 << (x))
+#define MASK(x) 											(1 << (x))
 
-const osThreadAttr_t thread_attr = {
-	.priority = osPriorityNormal1 
-};
-
-const osThreadAttr_t thread_attr_realtime6 = {
-	.priority = osPriorityRealtime6
-};
-
+// Set up Thread Priority 
 const osThreadAttr_t thread_attr_realtime7 = {
 	.priority = osPriorityRealtime7
 };
 
-// Type definition 
-
-typedef struct {
-	
-	uint8_t cmd;
-	uint8_t data;
-	
-}myDataPkt;
-
 typedef enum {
 	
-//	C7 = 2093,
-//	C7U = 2217,
-//	D7D = C7U,
-//	D7 = 2349,
-//	D7U = 2489,
-//	E7D = D7U,
-//	E7 = 2637,
-//	F7 = 2794,
-//	F7U = 2960,
-//	G7D = F7U,
-//	G7 = 3136,
-//	G7U = 3322,
-//	A7D = G7U,
-//	A7 = 3520,
-//	A7U = 3729,
-//	B7D = A7U,
-//	B7 = 3951,
+	 MoveForward , MoveBackward, TurnRight, TurnLeft, Stop
+	
+}Movement;
+
+// Type definition 
+typedef enum {
 	
 	C6 = 1047,
 	C6U = 1109,
@@ -122,20 +87,6 @@ typedef enum {
 }Note;
 
 typedef enum {
-	
-	RED_LED, GREEN_LED
-	
-}LED;
-
-
-
-typedef enum {
-	
-	 MoveForward , MoveBackward, TurnRight, TurnLeft, Stop
-	
-}Movement;
-
-typedef enum {
 	Whole = 64,
 	Half = 32,
 	Quarter = 16,
@@ -155,27 +106,33 @@ typedef enum {
 }NoteLength;
 
 Note song[] = {F6U, G6U, B6, B6, B6, B6, B6, B6, B6, F6U, G6U, B6, B6, B6, B6, B6, B6, B6, F6U, G6U, B6, B6, B6, B6, B6, B6, B6, B6, B6, B6D};
-NoteLength noteLength[] = {MEDIUM, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT,
-													 MEDIUM, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT,
-													 MEDIUM, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, MEDIUM,
-													 SHORT, SHORT, LONG};
-NoteLength silenceLength[] = {LONG, LONG, MEDIUM, MEDIUM, MEDIUM, MEDIUM, VERYSHORT, VERYSHORT, SHORT,
+	
+NoteLength noteLength[] = {	 	MEDIUM, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT,
+															MEDIUM, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT,
+															MEDIUM, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, MEDIUM,
+															SHORT, SHORT, LONG};
+
+NoteLength silenceLength[] ={ LONG, LONG, MEDIUM, MEDIUM, MEDIUM, MEDIUM, VERYSHORT, VERYSHORT, SHORT,
 															LONG, LONG, MEDIUM, MEDIUM, MEDIUM, MEDIUM, VERYSHORT, VERYSHORT, SHORT,
 															LONG, LONG, MEDIUM, MEDIUM, MEDIUM, MEDIUM, VERYSHORT, VERYSHORT, SHORT,
 															MEDIUM, MEDIUM, MEDIUM};
+
 Note connectTones[] = {C6, D6, E6, F6, G6, A6, B6};
 NoteLength connectNoteLength[] = {SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT};
 NoteLength connectSilenceLength[] = {SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT};
+
 Note finishTones[] = {B6, A6, G6, F6, E6, D6, C6};
 NoteLength finishNoteLength[] = {SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT};
 NoteLength finishSilenceLength[] = {SHORT, SHORT, SHORT, SHORT, SHORT, SHORT, SHORT};
 
 // Functions declaration
-void initMotorGPIO(int pin, char port, bool state);
-void initMotorPWM(int pin, char port);
 void initUART2(uint32_t baud_rate);
 void initPWM(void);
 void initGPIO(void);
+
+void initMotorGPIO(int pin, char port, bool state);
+void initMotorPWM(int pin, char port);
+
 void set_speed(float duty_cycle, bool update);
 void all_green_led_on(void);
 void all_green_led_off(void);
@@ -185,25 +142,21 @@ void red_led_250ms(void);
 
 
 void tMotorControl(Movement movement, float duty_cycle, bool update);
-void serial_decoder(int command);
+void tBrain(int command);
 void Serial_ISR(void);
+void tLED(void *argument);
+void tAudio(void *argument);
 
 // Global Variables
-
 volatile float current_duty_cycle = 0.5f;
 volatile Movement current_movement = Stop;
 volatile int CnV_value = 0;
-volatile int counter = 0;
-volatile int cnv = 0;
 volatile int led_on_id = 0;
+volatile bool isConnected = false;
+volatile int command;
+volatile bool isStarted = false;
+volatile bool finishedChallenge = false;
 
-
- static void delay(volatile uint32_t nof) {
-	while(nof != 0) {
-		__ASM("NOP");
-		nof--;
-	}
-}
 
 void initUART2(uint32_t baud_rate){
 	
@@ -262,32 +215,18 @@ void initPWM(void) {
 	// Configure MUX settings to make PTA4_Pin, PTB1_Pin, PTB3_PIN, PTC8_PIN, PTE30_PIN as PWM mode
 	PORTA->PCR[PTA4_PIN] &= ~PORT_PCR_MUX_MASK;
 	PORTA->PCR[PTA4_PIN] |= PORT_PCR_MUX(3);
-//	
-//	PORTB->PCR[PTB1_PIN] &= ~PORT_PCR_MUX_MASK;
-//	PORTB->PCR[PTB1_PIN] |= PORT_PCR_MUX(3);
-//	
-//	PORTB->PCR[PTB3_PIN] &= ~PORT_PCR_MUX_MASK;
-//	PORTB->PCR[PTB3_PIN] |= PORT_PCR_MUX(3);
-//	
-//	PORTC->PCR[PTC8_PIN] &= ~PORT_PCR_MUX_MASK;
-//	PORTC->PCR[PTC8_PIN] |= PORT_PCR_MUX(3);
-//	
-//	PORTE->PCR[PTE30_PIN] &= ~PORT_PCR_MUX_MASK;
-//	PORTE->PCR[PTE30_PIN] |= PORT_PCR_MUX(3);
-
-
 		
-		PORTE->PCR[PTE21_PIN] &= ~PORT_PCR_MUX_MASK;
-		PORTE->PCR[PTE21_PIN] |= PORT_PCR_MUX(3);
-		
-		PORTB->PCR[PTB1_PIN] &= ~PORT_PCR_MUX_MASK;
-		PORTB->PCR[PTB1_PIN] |= PORT_PCR_MUX(3);
-		
-		PORTA->PCR[PTA1_PIN] &= ~PORT_PCR_MUX_MASK;
-		PORTA->PCR[PTA1_PIN] |= PORT_PCR_MUX(3);
-		
-		PORTB->PCR[PTB2_PIN] &= ~PORT_PCR_MUX_MASK;
-		PORTB->PCR[PTB2_PIN] |= PORT_PCR_MUX(3);
+	PORTE->PCR[PTE21_PIN] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[PTE21_PIN] |= PORT_PCR_MUX(3);
+	
+	PORTB->PCR[PTB1_PIN] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[PTB1_PIN] |= PORT_PCR_MUX(3);
+	
+	PORTA->PCR[PTA1_PIN] &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[PTA1_PIN] |= PORT_PCR_MUX(3);
+	
+	PORTB->PCR[PTB2_PIN] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[PTB2_PIN] |= PORT_PCR_MUX(3);
 	
 	
 	// Enable clock to supply power to TPM0, TPM1 and TPM2
@@ -296,7 +235,6 @@ void initPWM(void) {
   SIM->SCGC6 |= SIM_SCGC6_TPM2_MASK;
 	
 
-
 	// SIM->SOPT2 --> System Options Register
 	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK; 	//Ensure TPMSRC bit in SOPT2 is cleared before assigning a new value
 	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1);	//Assign 1 to select s MCGFLLCLK clock or MCGPLLCLK/2 as clock source for the TPM clock
@@ -304,10 +242,6 @@ void initPWM(void) {
 	// Set all TPM values to 0 to prevent the motor from running at the very beginning after running the program
 	TPM0->MOD = 0;
 	TPM0_C1V = 0;
-//	TPM0_C2V = 0;
-//	TPM0_C3V = 0;
-//	TPM0_C4V = 0;
-//	TPM0_C5V = 0;
 
 	TPM1->MOD = 7500;
 	TPM1_C0V = 0;
@@ -322,10 +256,6 @@ void initPWM(void) {
 	TPM2->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK)); 	//Clear before assign value
 	
 	TPM0_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK)); // Clear Channel 1 before assign
-//	TPM0_C2SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK)); // Clear Channel 2 before assign
-//	TPM0_C3SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK)); // Clear Channel 3 before assign
-//	TPM0_C4SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK)); // Clear Channel 4 before assign
-//	TPM0_C5SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK)); // Clear Channel 5 before assign
 	
 	//CMOD:LPTPM counter increments on every LPTPM counter clock
 	//PS:111(7) Divide by 128
@@ -347,10 +277,6 @@ void initPWM(void) {
 
 	// ELSB:ELSA = 10 , MSB:MSA = 10  --> Edge-aligned PWM, High-true pulses (clear Output on match, set Output on reload)
 	TPM0_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));  // For TPM0 Channel 1  
-//	TPM0_C2SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1)); 	// For TPM0 Channel 2
-//	TPM0_C3SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));  // For TPM0 Channel 3
-//	TPM0_C4SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));  // For TPM0 Channel 4  
-//	TPM0_C5SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1)); 	// For TPM0 Channel 5
 
 	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));  // For TPM1 Channel 0  
 	TPM1_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1)); 	// For TPM1 Channel 1
@@ -372,22 +298,12 @@ void initGPIO(void) {
 	PORTA->PCR[PTA2_PIN] &= ~PORT_PCR_MUX_MASK;
 	PORTA->PCR[PTA2_PIN] |= PORT_PCR_MUX(1);
 	
+	PORTA->PCR[PTA13_PIN] &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[PTA13_PIN] |= PORT_PCR_MUX(1);
+	
 	PORTB->PCR[PTB3_PIN] &= ~PORT_PCR_MUX_MASK;
   PORTB->PCR[PTB3_PIN] |= PORT_PCR_MUX(1);
 	
-	// Configure MUX settings to GPIO    
-//	PORTA->PCR[PTA5_PIN] &= ~PORT_PCR_MUX_MASK;  
-//	PORTA->PCR[PTA5_PIN] |= PORT_PCR_MUX(1); 	
-//	
-//	PORTA->PCR[PTA13_PIN] &= ~PORT_PCR_MUX_MASK;  
-//	PORTA->PCR[PTA13_PIN] |= PORT_PCR_MUX(1); 
-//	
-//	PORTB->PCR[PTB0_PIN] &= ~PORT_PCR_MUX_MASK;  
-//	PORTB->PCR[PTB0_PIN] |= PORT_PCR_MUX(1);
-// 
-//	PORTB->PCR[PTB2_PIN] &= ~PORT_PCR_MUX_MASK;  
-//	PORTB->PCR[PTB2_PIN] |= PORT_PCR_MUX(1);
-//	
 	PORTB->PCR[PTB8_PIN] &= ~PORT_PCR_MUX_MASK;  
 	PORTB->PCR[PTB8_PIN] |= PORT_PCR_MUX(1);
 	 
@@ -412,47 +328,14 @@ void initGPIO(void) {
 	PORTD->PCR[PTD5_PIN] &= ~PORT_PCR_MUX_MASK;  
 	PORTD->PCR[PTD5_PIN] |= PORT_PCR_MUX(1);
 	
-	// Set Data Direction Registers for Port A, Port B, Port C and Port D  
-//	PTA->PDDR |= (MASK(PTA5_PIN) | MASK(PTA13_PIN));
-//	PTB->PDDR |= (MASK(PTB0_PIN) | MASK(PTB2_PIN) | MASK(PTB8_PIN));    
-//	PTC->PDDR |= (MASK(PTC9_PIN) | MASK(PTC12_PIN) | MASK(PTC13_PIN) | MASK(PTC16_PIN) | MASK(PTC17_PIN));
-//	PTD->PDDR |= (MASK(PTD0_PIN) | MASK(PTD2_PIN) | MASK(PTD5_PIN));
-//	
-//	PTA->PDOR &= ~MASK(PTA5_PIN);
-//	PTA->PDOR &= ~MASK(PTA13_PIN);
-//	PTB->PDOR &= ~MASK(PTB0_PIN);
-//	PTB->PDOR &= ~MASK(PTB2_PIN);
-//	PTB->PDOR &= ~MASK(PTB8_PIN);
-//	PTC->PDOR &= ~MASK(PTC9_PIN);	
-//	PTC->PDOR &= ~MASK(PTC12_PIN);	
-//	PTC->PDOR &= ~MASK(PTC13_PIN);
-//	PTC->PDOR &= ~MASK(PTC16_PIN);
-//	PTC->PDOR &= ~MASK(PTC17_PIN);
-//	PTD->PDOR &= ~MASK(PTD0_PIN);
-//	PTD->PDOR &= ~MASK(PTD2_PIN);
-//	PTD->PDOR &= ~MASK(PTD5_PIN);
 
-	PTA->PDDR |= (MASK(PTA2_PIN));
+	PTA->PDDR |= (MASK(PTA2_PIN) | MASK(PTA13_PIN));
 	PTB->PDDR |= (MASK(PTB1_PIN) | MASK(PTB3_PIN) | MASK(PTB8_PIN));    
 	PTC->PDDR |= (MASK(PTC12_PIN) | MASK(PTC13_PIN) | MASK(PTC16_PIN) | MASK(PTC17_PIN));
 	PTD->PDDR |= (MASK(PTD0_PIN) | MASK(PTD2_PIN) | MASK(PTD5_PIN));
-	
-//	PTA->PDOR &= ~MASK(PTA5_PIN);
-//	PTA->PDOR &= ~MASK(PTA13_PIN);
-//	PTB->PDOR &= ~MASK(PTB0_PIN);
-//	PTB->PDOR &= ~MASK(PTB2_PIN);
-//	PTB->PDOR &= ~MASK(PTB8_PIN);
-//	PTC->PDOR &= ~MASK(PTC9_PIN);	
-//	PTC->PDOR &= ~MASK(PTC12_PIN);	
-//	PTC->PDOR &= ~MASK(PTC13_PIN);
-//	PTC->PDOR &= ~MASK(PTC16_PIN);
-//	PTC->PDOR &= ~MASK(PTC17_PIN);
-//	PTD->PDOR &= ~MASK(PTD0_PIN);
-//	PTD->PDOR &= ~MASK(PTD2_PIN);
-//	PTD->PDOR &= ~MASK(PTD5_PIN);
 
 	PTA->PDOR &= ~MASK(PTA2_PIN);
-	//PTA->PDOR &= ~MASK(PTA13_PIN);
+	PTA->PDOR &= ~MASK(PTA13_PIN);
 	PTB->PDOR &= ~MASK(PTB1_PIN);
 	PTB->PDOR &= ~MASK(PTB3_PIN);
 	PTB->PDOR &= ~MASK(PTB8_PIN);
@@ -463,6 +346,5 @@ void initGPIO(void) {
 	PTD->PDOR &= ~MASK(PTD0_PIN);
 	PTD->PDOR &= ~MASK(PTD2_PIN);
 	PTD->PDOR &= ~MASK(PTD5_PIN);
-
 
 }
--- CG2271 Project/main.c
+++ CG2271 Project/main.c
@@ -1,23 +1,8 @@
 #include "myheader.h"
 
-//volatile osSemaphoreId_t mySemMoveForward;
-//volatile osSemaphoreId_t mySemMoveBackward;
-//osMutexId_t myMutex;
-
-//osThreadId_t tMotorControl_Id;
-
-volatile bool isConnected = false;
-
-volatile int command;
-
-volatile int debug = 0;
-
-volatile int debug2 = 0;
-
-volatile bool isStarted = false;
-
-volatile bool finishedChallenge = false;
-
+/*----------------------------------------------------------------------------
+ * Motor Control Code
+ *---------------------------------------------------------------------------*/
 void initMotorGPIO(int pin, char port, bool state) {
 	
 	switch (port) {
@@ -49,21 +34,6 @@ void initMotorGPIO(int pin, char port, bool state) {
 			}
 			break;
 			
-//		case 'C':
-//			
-//			PORTC->PCR[pin] &= ~PORT_PCR_MUX_MASK;  
-//			PORTC->PCR[pin] |= PORT_PCR_MUX(1);
-//		
-//			PTC->PDDR |=  MASK(pin);
-//		
-//			if (state) {
-//				PTC->PDOR |= MASK(pin);
-//			}else {
-//				PTC->PDOR &= ~MASK(pin);
-//			}
-//			
-//			break;
-			
 		case 'E':
 	
 			PORTE->PCR[pin] &= ~PORT_PCR_MUX_MASK;  
@@ -76,7 +46,6 @@ void initMotorGPIO(int pin, char port, bool state) {
 			}else {
 				PTE->PDOR &= ~MASK(pin);
 			}
-			
 			break;
 	}
 }
@@ -108,16 +77,6 @@ void initMotorPWM(int pin, char port) {
 		
 			break;
 		
-//		case 'C':
-//		
-//			PORTC->PCR[pin] &= ~PORT_PCR_MUX_MASK;  
-//			PORTC->PCR[pin] |= PORT_PCR_MUX(3);
-//		
-//			TPM0_C4V = (int)(TPM0->MOD * current_duty_cycle);
-//			TPM0_C5V = (int)(TPM0->MOD * current_duty_cycle);
-//			
-//			break;
-		
 		case 'E':
 		
 			PORTE->PCR[pin] &= ~PORT_PCR_MUX_MASK;  
@@ -134,7 +93,7 @@ void set_speed(float duty_cycle, bool update) {
 	
 	if (update) {
 		current_duty_cycle = duty_cycle;
-	} 
+	}
 	
 	CnV_value = (int)(TPM1->MOD * duty_cycle);
 	
@@ -142,16 +101,20 @@ void set_speed(float duty_cycle, bool update) {
 	TPM1_C1V = CnV_value;
 	TPM2_C0V = CnV_value;
 	TPM2_C1V = CnV_value;
+
 }
+
+
 
 void tMotorControl(Movement movement, float duty_cycle, bool update) {
 	
 	current_movement = movement;
-	set_speed(duty_cycle, update);
+	
 	
 	switch (movement) {
 		
 		case MoveForward:
+			set_speed(duty_cycle, update);
 			// FrontLeftWheel (Move Forward)
 			initMotorGPIO(PTA1_PIN, 'A', false);
 			initMotorPWM(PTA2_PIN, 'A');
@@ -171,7 +134,7 @@ void tMotorControl(Movement movement, float duty_cycle, bool update) {
 			break;
 		
 		case MoveBackward:
-			
+			set_speed(duty_cycle, update);
 			// FrontLeftWheel (Move Backward)
 			initMotorGPIO(PTA2_PIN, 'A', false);
 			initMotorPWM(PTA1_PIN, 'A');
@@ -191,7 +154,7 @@ void tMotorControl(Movement movement, float duty_cycle, bool update) {
 			break;
 		
 		case TurnLeft:
-			
+			 set_speed(duty_cycle, update);
 			// FrontRightWheel (Move Forward)
 			initMotorGPIO(PTB1_PIN, 'B', false);
 			initMotorPWM(PTB0_PIN, 'B');
@@ -207,11 +170,14 @@ void tMotorControl(Movement movement, float duty_cycle, bool update) {
 			// BackLeftWheel (Move Backward)
 			initMotorGPIO(PTB3_PIN, 'B', false);
 			initMotorPWM(PTB2_PIN, 'B');
+		
+			
 			
 			break;
 		
 		case TurnRight:
 			
+			set_speed(duty_cycle, update);
 			// FrontLeftWheel (Move Forward)
 			initMotorGPIO(PTA1_PIN, 'A', false);
 			initMotorPWM(PTA2_PIN, 'A');
@@ -227,6 +193,8 @@ void tMotorControl(Movement movement, float duty_cycle, bool update) {
 			// BackRightWheel (Move Backward)
 			initMotorGPIO(PTE20_PIN, 'E', false);
 			initMotorPWM(PTE21_PIN, 'E');	
+			
+			
 		
 			break;
 		
@@ -251,80 +219,53 @@ void tMotorControl(Movement movement, float duty_cycle, bool update) {
 
 
 
-void serial_decoder(int command) {
+void tBrain(int command) {
 	
 	if (command < LOWERCOMMANDLIMIT || command > UPPERCOMMANDLIMIT) {
 		
-		if (command == 100) {
+		if (command == CONNECTED) {
+			
 			isConnected = true;
-//			myData.cmd = 0x03;
-//			myData.data = 0x03;
-		} else if (command == 200) {
+			
+		} else if (command == FINISHED) {
+			
 			finishedChallenge = true;
-//			myData.cmd = 0x02;
-//			myData.data = 0x02;
-		} else if (command == 255) {
+			
+		} else if (command == STARTED) {
+			
 			isStarted = true;
 		}
 		return;
 	}
 	
 	if (command < 11) { // duty_cycle control
-//		myData.cmd = 0x02;
-//		myData.data = 0x02;
 		set_speed(command / 10.0f, true);
 
 	} else if (command == 11) {
-			debug++;
-//			myData.cmd = 0x01;
-//			myData.data = 0x01;
+		
 			tMotorControl(MoveForward, current_duty_cycle, true);
 
-
 	} else if (command == 12) { 
-//			myData.cmd = 0x01;
-//			myData.data = 0x01;
+		
 			tMotorControl(MoveBackward, current_duty_cycle, true);
 
 	} else if (command == 13) { 
-//			myData.cmd = 0x01;
-//			myData.data = 0x01;
+		
 			tMotorControl(TurnRight, current_duty_cycle, true);
-
 		
 	} else if (command == 14) {
-//			myData.cmd = 0x01;
-//			myData.data = 0x01;
+		
 			tMotorControl(TurnLeft, current_duty_cycle, true);
 
-
 	} else {
-//			myData.cmd = 0x02;
-//			myData.data = 0x02;
+		
 			tMotorControl(Stop, current_duty_cycle, false);
 	}
 }
 
-/* UART2 Receive Poll*/
-void Serial_ISR(void) {
-	
-		if((UART2->S1 & UART_S1_RDRF_MASK)) {	
-	
-			command = UART2->D;
-			serial_decoder(command);
-		}
-		
-}
- 
-void UART2_IRQHandler(void){
-	NVIC_ClearPendingIRQ(UART2_IRQn);
-	Serial_ISR();
-}
 
 
-void tBrain () {
-	serial_decoder(command);
-}
+
 
 /*----------------------------------------------------------------------------
  * Red LED and Green LED Code 
@@ -332,12 +273,8 @@ void tBrain () {
 
 void tLED(void *argument) {
 	
-	//myDataPkt myRxData;
-	
 	for (;;) {
-		
-		//osMessageQueueGet(runningLedMsg, &myRxData, NULL, osWaitForever);
-		//debug2++;
+	
 		if (current_movement == Stop) {
 			red_led_250ms();
 			if (!isConnected) {
@@ -347,96 +284,69 @@ void tLED(void *argument) {
 				isConnected = false;
 			}
 		} else {
+				red_led_500ms();
+				all_green_led_off();
 			
-			red_led_500ms();
-			all_green_led_off();
-			
-			if (led_on_id > 7) {
-				led_on_id = 0;
-			}
-	
-			switch (led_on_id) {
-				case 0:
-					PTC->PDOR &= ~MASK(PTC17_PIN);
-					PTA->PDOR |= MASK(PTA13_PIN);
-					//osDelay(1);
-					
-					break;
-				case 1:
-					PTA->PDOR &= ~MASK(PTA13_PIN);
-					PTC->PDOR |= MASK(PTC12_PIN);
-					//osDelay(1);
-					
-					break;
-				case 2:
-					PTC->PDOR &= ~MASK(PTC12_PIN);
-					PTD->PDOR |= MASK(PTD5_PIN);
-					//osDelay(1);
-					break;
-				
-				case 3:
-					PTD->PDOR &= ~MASK(PTD5_PIN);
-					PTC->PDOR |= MASK(PTC13_PIN);
-					//osDelay(1);
-					break;
-				case 4:
-					PTC->PDOR &= ~MASK(PTC13_PIN);
-					PTD->PDOR |= MASK(PTD0_PIN);
-					//osDelay(1);
-					break;
-				case 5:
-					PTD->PDOR &= ~MASK(PTD0_PIN);
-					PTC->PDOR |= MASK(PTC16_PIN);
-					//osDelay(1);
-					break;
-				case 6:
-					PTC->PDOR &= ~MASK(PTC16_PIN);
-					PTD->PDOR |= MASK(PTD2_PIN);
-					//osDelay(1);
-					break;
-				case 7:
-					PTD->PDOR &= ~MASK(PTD2_PIN);
-					PTC->PDOR |= MASK(PTC17_PIN);
-					//osDelay(1);
-					break;	
-			}
-			
+				if (led_on_id > 7) {
+					led_on_id = 0;
+				}
 		
-			led_on_id++;
-			
+				switch (led_on_id) {
+					
+					case 0:
+						PTC->PDOR &= ~MASK(PTC17_PIN);
+						PTA->PDOR |= MASK(PTA13_PIN);
+					
+						
+						break;
+					case 1:
+						PTA->PDOR &= ~MASK(PTA13_PIN);
+						PTC->PDOR |= MASK(PTC12_PIN);
+					
+						
+						break;
+					case 2:
+						PTC->PDOR &= ~MASK(PTC12_PIN);
+						PTD->PDOR |= MASK(PTD5_PIN);
+						
+						break;
+					
+					case 3:
+						PTD->PDOR &= ~MASK(PTD5_PIN);
+						PTC->PDOR |= MASK(PTC13_PIN);
+						
+						break;
+					case 4:
+						PTC->PDOR &= ~MASK(PTC13_PIN);
+						PTD->PDOR |= MASK(PTD0_PIN);
+						
+						break;
+					case 5:
+						PTD->PDOR &= ~MASK(PTD0_PIN);
+						PTC->PDOR |= MASK(PTC16_PIN);
+					
+						break;
+					case 6:
+						PTC->PDOR &= ~MASK(PTC16_PIN);
+						PTD->PDOR |= MASK(PTD2_PIN);
+					
+						break;
+					case 7:
+						PTD->PDOR &= ~MASK(PTD2_PIN);
+						PTC->PDOR |= MASK(PTC17_PIN);
+				
+						break;	
+			}		
+		
+			led_on_id++;		
 		}
 		osDelay(100);
 		
 	}
-
 }
 
-//void green_led_control (void *arguement) {
-//	
-//	myDataPkt myRxData;
-//	
-//	for (;;) {
-//		
-//		//osMessageQueueGet(runningLedMsg, &myRxData, NULL, osWaitForever);
-//		//debug2++;
-//		//if (myRxData.cmd == 0x02 && myRxData.data == 0x02) {
-////		if (current_movement == Stop) {
-////			red_led_250ms();
-////			all_green_led_on();
-////		} 
-//		//else if (myRxData.cmd == 0x03 && myRxData.data == 0x03 && isConnected == false ) {
-////		else if (isConnected == false)
-////			isConnected = true;
-////			double_flash_green_led();
-////			// play unique tone here 
-////			
-////		}
-//		osDelay(100);
-//	}
-
-//}
-
 void all_green_led_on (void) {
+	
 	PTD->PDOR |= MASK(PTD5_PIN);
 	PTD->PDOR |= MASK(PTD0_PIN);
 	PTD->PDOR |= MASK(PTD2_PIN);
@@ -448,6 +358,7 @@ void all_green_led_on (void) {
 }
 
 void all_green_led_off (void) {
+	
 	PTD->PDOR &= ~MASK(PTD0_PIN);
 	PTD->PDOR &= ~MASK(PTD2_PIN);
 	PTD->PDOR &= ~MASK(PTD5_PIN);
@@ -461,6 +372,7 @@ void all_green_led_off (void) {
 
 
 void double_flash_green_led(void) {
+	
 	all_green_led_off();
 	osDelay(200);
 	all_green_led_on();
@@ -471,10 +383,6 @@ void double_flash_green_led(void) {
 	osDelay(200);
 }
 	
-
-
-
-
 void red_led_500ms(void) {
 	
 	PTB->PTOR |= MASK(PTB8_PIN);
@@ -487,39 +395,30 @@ void red_led_250ms(void) {
 	osDelay(250);
 }
 
-//void tLED (void *argument) {
-//	
-//	
-//	for(;;) {
-//		
-//		osMessageQueuePut(runningLedMsg, &myData, NULL, 0);
-//		osDelay(100);
-//		osMessageQueuePut(allGreenLedOnMsg, &myData, NULL, 0);
-//		osDelay(100);
-//	
-//	}
-//	
-//}
-
 /*----------------------------------------------------------------------------
  * Buzzer Code 
  *---------------------------------------------------------------------------*/
 void playnote(Note note, NoteLength length, int bpm) {
+	
 	if (note != 0) {
+		
 		TPM0->MOD = (int)(48000000 / (128 * note));
 		TPM0_C1V = TPM0->MOD >> 1;
 	} else {
+		
 		TPM0_C1V = 0;
 	}
 	osDelay((int)((float)length / MEDIUM * 60000 / bpm));
 }
 
 void tAudio(void *argument) {
+	
 	int nofNotes = sizeof(song) / sizeof(song[0]);
 	int nofNotes1 = sizeof(finishTones) / sizeof(finishTones[0]);
 	int nofNotes2 = sizeof(connectTones) / sizeof(connectTones[0]);
 	
 	for (;;) {
+		
 		if (isStarted) {
 			for (int i = 0; i < nofNotes && !isConnected && !finishedChallenge; ++i) {
 				playnote(song[i], noteLength[i], 240);
@@ -527,14 +426,15 @@ void tAudio(void *argument) {
 			}
 		}
 		if (finishedChallenge) {
+			
 			for (int i = 0; i < nofNotes1; ++i) {
 				playnote(finishTones[i], finishNoteLength[i], 240);
 				playnote(SILENCE, finishSilenceLength[i], 240);
 			}
-//			finishedChallenge = false;
 			break;
 		}
 		if (isConnected) {
+			
 			for (int i = 0; i < nofNotes2; ++i) {
 				playnote(connectTones[i], connectNoteLength[i], 240);
 				playnote(SILENCE, finishNoteLength[i], 240);
@@ -549,6 +449,25 @@ void tAudio(void *argument) {
 	
 }
 
+/*----------------------------------------------------------------------------
+ * ISR and UART Controller
+ *---------------------------------------------------------------------------*/
+void Serial_ISR(void) {
+	
+		if((UART2->S1 & UART_S1_RDRF_MASK)) {	
+	
+			command = UART2->D;
+			tBrain(command);
+		}
+		
+}
+ 
+void UART2_IRQHandler(void){
+	
+	NVIC_ClearPendingIRQ(UART2_IRQn);
+	Serial_ISR();
+}
+
 
  
 int main (void) {
@@ -561,15 +480,6 @@ int main (void) {
 	initUART2(BAUD_RATE);
 	
   osKernelInitialize();                 // Initialize CMSIS-RTOS
-	
-//	runningLedMsg = osMessageQueueNew(MSG_COUNT, sizeof(myDataPkt), NULL);
-//	allGreenLedOnMsg = osMessageQueueNew(MSG_COUNT, sizeof(myDataPkt), NULL);
-	
-	//osThreadNew(tBrain, NULL, &thread_attr_realtime7);
-	
-	//osThreadNew(tLED, NULL, &thread_attr_realtime7);
-	
-	//osThreadNew(green_led_control, NULL, &thread_attr_realtime7);
 	
 	osThreadNew(tLED, NULL, &thread_attr_realtime7);
 	
